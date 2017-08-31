9e8a5054418fc3439edae916cea36074b890fe37
ben lamothe <zonotope@gmail.com>
2017-08-30 21:26:53 -0400

    initial commit

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject questlog "0.1.0-SNAPSHOT"
+  :description "Ring request logging middleware"
+
+  :url "http://example.com/FIXME"
+
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.logging "0.4.0"]])
